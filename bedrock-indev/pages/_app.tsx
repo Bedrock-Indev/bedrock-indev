e8d5611fce3b98e89892d214b05581237da14205
Daniel Ryan <ryannobusiness@gmail.com>
2023-07-01 21:13:49 +0100

    reconfiguring the project

---
+++ bedrock-indev/pages/_app.tsx
@@ -0,0 +1,10 @@
+'use client'
+import '../styles/globals.css'
+import '../styles/tailwind.css'
+import AppProps from 'next/app';
+
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default App;
